bcb4012777c787a9d518a3f8b727d1608d5f4847
PGKing529 <529408806@qq.com>
2022-07-18 16:41:01 +0800

    lesson1

--- lesson1/gorm.go
+++ lesson1/gorm.go
@@ -31,11 +31,21 @@ func main() {
 
 	// Read
 	var user User
-	db.First(&user, 1) // find user with id 1
+	db.First(&user, 1)                    // find user with id 1
+	db.First(&user, "name = ?", "jinzhu") // find user with name jinzhu
 	fmt.Println(user.Name)
 
 	// Update - update user's name to "jinzhu"
-	db.Model(&user).Update("Name", "jinzhu")
+	db.Model(&user).Update("Name", "yinzhu")
+
+	// Update - update user's name to "jinzhu" and age to 20
+	db.Model(&user).Update("Name", "yyz").Update("Age", 20)
+
+	// update - update multiple fields
+	db.Model(&user).Updates(map[string]interface{}{"Name": "multiple", "Age": 20})
+
+	// update - update multiple fields with struct
+	db.Model(&user).Updates(&User{Name: "struct", Age: 20})
 
 	// // Delete - delete user with id 1
 	// db.Delete(&user, 1)
